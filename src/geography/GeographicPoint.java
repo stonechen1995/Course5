d824123af061cc8ec81d032dad024b4935b79019
Stone Chen <stonechen1995@gmail.com>
2021-07-19 07:13:36 -0400

    AStarSearch finished
    This reverts commit 7736fb6d3c556455bd1e00a829026efb6753a532.

--- src/geography/GeographicPoint.java
+++ src/geography/GeographicPoint.java
@@ -39,6 +39,15 @@ public class GeographicPoint extends Double {
     	double d = R * c;
     	return d;
     }
+	
+	private double getDist2(double lat1, double lon1, double lat2, double lon2)
+    {
+//		System.out.println(Math.pow(lat2 - lat1, 2));
+//		System.out.println(Math.pow(lon2 - lon1, 2));
+//		System.out.println(Math.sqrt(Math.pow(lat2 - lat1, 2) + Math.pow(lon2 - lon1, 2)));
+    	double d = Math.sqrt(Math.pow(lat2 - lat1, 2) + Math.pow(lon2 - lon1, 2));
+    	return d;
+    }
     
     public String toString()
     {
