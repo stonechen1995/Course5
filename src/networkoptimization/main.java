c6ff1d58dcfe18fadee6157f85ab4234538289cc
Stone Chen <stonechen@tamu.edu>
2021-11-28 11:44:05 -0600

    minor updated with the main class to test the first two Dijkstra versions

--- src/networkoptimization/main.java
+++ src/networkoptimization/main.java
@@ -4,23 +4,33 @@ import java.util.ArrayList;
 
 class main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static void test1() {
 		GraphGenerator test1 = new GraphGenerator();
 		
-		Graph sparseGraph = test1.generateSparseGraph(5000, 6);
-//		Graph denseGraph = test1.generateDenseGraph(5000, 20);
+		Graph sparseGraph = test1.generateSparseGraph(100, 6);
+		Graph denseGraph = test1.generateDenseGraph(100, 20);
 		
 //		sparseGraph.printGraph();
 		int result1 = 0;
 		int result2 = 0;
-		while(result1 == result2) {
+		int result3 = 0;
+		int result4 = 0;
+		int count = 0;
+		while(result1 == result2 && result3 == result4 && count++ < 10000) {
 //			System.out.println("=====================================");
-			sparseGraph = test1.generateSparseGraph(20, 4);
+			sparseGraph = test1.generateSparseGraph(100, 6);
 			result1 = sparseGraph.findMBPath_Heap(8, 9);
 			result2 = sparseGraph.findMBPath_Baisc(8, 9);
+			sparseGraph = test1.generateDenseGraph(100, 20);
+			result3 = denseGraph.findMBPath_Heap(8, 9);
+			result4 = denseGraph.findMBPath_Baisc(8, 9);
 //			break;
 		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		test1();
 		
 //		denseGraph.findMBPath_Heap(8, 9);
 //		denseGraph.findMBPath_Baisc(8, 9);
